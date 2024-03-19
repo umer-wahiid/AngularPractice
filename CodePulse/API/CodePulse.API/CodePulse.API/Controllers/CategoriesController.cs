0ff8aca7b2ad3e3b20992e0734936058645528e9
Umer Abdul Wahid & Ammar Ansari <umerwahiid48@gmail.com>
2024-03-20 04:38:17 +0500

    Create Category Method

---
+++ CodePulse/API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,43 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this._context = dbContext;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            var category = new Models.Domain.Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+    }
+}
--- CodePulse/API/CodePulse.API/CodePulse.API/obj/Debug/net8.0/CodePulse.API.AssemblyInfo.cs
+++ CodePulse/API/CodePulse.API/CodePulse.API/obj/Debug/net8.0/CodePulse.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+500b004cb30dfea8a1efdadf34de9c72d493dbfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
