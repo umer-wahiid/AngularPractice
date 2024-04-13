0c703e66c613a1dc9ce55625f8678cdb724fdd7b
Umer Abdul Wahid & Ammar Ansari <umerwahiid48@gmail.com>
2024-04-14 00:31:33 +0500

    Categories Component Added

--- CodePulse/UI/codepulse/src/app/app.routes.ts
+++ CodePulse/UI/codepulse/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './features/category/category-list/category-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'admin/categories',
+        component: CategoryListComponent
+    }
+];
+
+// @NgModule({
+//     imports: [RouterModule.forRoot(routes)],
+//     exports: [RouterModule]
+// })
+// export class AppRoutes{ }
